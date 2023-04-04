ab835ab3b9f9e2601a24c7580bcd68183e6015d4
MILLENA FERREIRA DE SOUSA <22282227@dsn127m1242374.cfp127aluno.local>
2023-04-04 16:51:20 -0300

    user

---
+++ app/src/main/java/br/senai/sp/jandira/mylogin/dao/repository/UserDao.kt
@@ -0,0 +1,27 @@
+package br.senai.sp.jandira.mylogin.dao.repository
+
+import androidx.room.*
+import br.senai.sp.jandira.mylogin.model.User
+
+//Contrato
+@Dao
+interface UserDao {
+    //salvar o usuario no banco de dados
+    @Insert
+    fun save(user: User): Long
+
+    //atualizar o usuario no banco
+    //esse int me mostra quantos usuarios seram atualizados no banco
+    @Update
+    fun update(user: User): Int
+
+    //deletar o usuario do banco
+    @Delete
+    fun delete(user: User): Int
+
+    //função para buscar usuario que esta se cadastrando pelo email no banco
+    //se o email nao estiver cadastrado cria-se um novo usuario
+    @Query("SELECT * FROM tbl_user WHERE email = :email")
+    fun findUserByEmail(email: String): User
+
+}
---
+++ app/src/main/java/br/senai/sp/jandira/mylogin/reposity/UserRepository.kt
@@ -0,0 +1,15 @@
+package br.senai.sp.jandira.mylogin.reposity
+
+import android.content.Context
+import br.senai.sp.jandira.mylogin.dao.repository.TripDb
+import br.senai.sp.jandira.mylogin.model.User
+
+class UserRepository(context: Context) {
+
+    //banco de dados
+    private val db = TripDb.getDatabase(context)
+
+    fun save(user: User): Long{
+        return db.userDao().save(user)
+    }
+}
